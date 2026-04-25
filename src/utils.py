ed1e8eca066d3b4695232e6ab227541b83112fcb
Yug Dalal <yugdalal97@gmail.com>
2026-04-25 14:54:29 +0530

    HyperParameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ import pandas as pd
 import dill 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
+import warnings
+warnings.filterwarnings('ignore')
 
 
 from src.exception import CustomException
@@ -21,14 +24,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
             
+            
+            rs=RandomizedSearchCV(model, para, cv=3, n_iter=10, random_state=42, n_jobs=-1,verbose=False)
+            rs.fit(X_train,y_train)
+            
+            model.set_params(**rs.best_params_)
             model.fit(X_train,y_train)
+            # model.fit(X_train,y_train)
             
             y_train_pred = model.predict(X_train)
             
@@ -39,6 +49,6 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             
             report[list(models.keys())[i]]= test_model_score
             
-            return report
-    except:
-        pass
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
